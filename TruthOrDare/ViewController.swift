49c23aa241d48d36f034644da72feac20de550eb
SergeiRyazanov <ryazanovsergei15@gmail.com>
2026-06-18 13:59:19 +0600

    first commit

---
+++ TruthOrDare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TruthOrDare
+//
+//  Created by Сергей Рязанов on 6/18/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
